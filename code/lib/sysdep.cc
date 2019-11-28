8dbf14488dc4b8dcaded24f39220e4f86ce1accd
wsxst <280373133@qq.com>
2019-11-28 13:39:50 +0800

    Some little modification

--- code/lib/sysdep.cc
+++ code/lib/sysdep.cc
@@ -235,9 +235,7 @@ AllocBoundedArray(int size)
 #ifdef NO_MPROT
 void DeallocBoundedArray(char *ptr, int /* size */)
 {
-    // cout<<"哈哈3"<<endl;
     delete[] ptr;
-    // cout<<"哈哈4"<<endl;
 }
 #else
 void DeallocBoundedArray(char *ptr, int size)
--- code/test/matmult.c
+++ code/test/matmult.c
@@ -34,5 +34,5 @@ main()
             for (k = 0; k < Dim; k++)
 		 C[i][j] += A[i][k] * B[k][j];
 
-    // Exit(C[Dim-1][Dim-1]);		/* and then we're done */
+    Exit(C[Dim-1][Dim-1]);		/* and then we're done */
 }
